5fa0a57f67316d3cb35dfda828b3da37800c6e49
缪尔赛思\86187 <e1156729@u.nus.edu>
2025-01-31 16:14:49 +0800

    Level-0 hello

---
+++ src/main/java/Lys.java
@@ -0,0 +1,17 @@
+public class Lys {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        String hello = "____________________________________________________________\n" +
+                "Hello! I'm [YOUR CHATBOT NAME]\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(hello);
+    }
+}
